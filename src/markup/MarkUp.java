9d3561dbb7970f4f76554f6383044d297940140a
WrightArchitectural <WrightArchitectural@192.168.0.13>
2015-08-28 18:29:05 -0700

    Modify logic

--- src/markup/MarkUp.java
+++ src/markup/MarkUp.java
@@ -29,12 +29,35 @@ public class MarkUp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO Declare & instanciate LineItem object
+        // Declare, instanciate LineItem object & variables
+        LineItem li0;
+        double initialValue;
+        double percent;
         
-        // TODO CLI
+        // Formating objects
+        DecimalFormat currencyFormat = new DecimalFormat( "$ #,#00.00" );
         
-        // TODO Applciation logic
+        // CL inputs
+        Scanner scan = new Scanner( System.in );
+        System.out.println( "Line Item MarkUp Utility" );
+        System.out.println( "--------------------------------------" );
         
+        System.out.println( "Please enter initial line item cost > " );
+        initialValue = scan.nextDouble();
+        System.out.println( "Line initial value: " + currencyFormat.format( initialValue ) );
+        
+        System.out.println( "Please enter percent markup > " );
+        percent = scan.nextDouble();
+        System.out.println( "Percent markup: " + percent + " %");
+        
+        // Applciation logic
+        li0 = new LineItem( initialValue, percent );
+        li0.markupCalc();
+        
+        // CL outputs
+        System.out.println( "--------------------------------------" );
+        System.out.println( "Line item marked: " + currencyFormat.format( li0.getLIMarked() ) );
+
     }
     
 }
